8970ebc9e0fabad5bfff0dd3a7c2d375a413cb66
Junil Hong <grt1022@gamil.com>
2023-09-24 00:20:20 +0900

    gsap_Box_Animation

---
+++ gsap_Box_Animation/gsap_Box_Animation.js
@@ -0,0 +1,82 @@
+gsap.to('.boxes', {
+    ease: 'power4.inOut',
+    delay: .7,
+    duration: 1,
+    display: 'block',
+})
+
+gsap.to('.box1', {
+    ease: 'power4.inOut',
+    delay: .5,
+    duration: 1,
+    height: '40vh',
+})
+
+gsap.to('.box2', {
+    ease: 'power4.inOut',
+    delay: .5,
+    duration: 1,
+    height: '40vh',
+})
+
+gsap.to('.box1', {
+    ease: 'power4.inOut',
+    delay: 1.3,
+    duration: 1,
+    width: '43vw',
+})
+
+gsap.to('.box2', {
+    ease: 'power4.inOut',
+    delay: 1.3,
+    duration: 1,
+    width: '43vw',
+})
+
+gsap.to('.text', {
+    autoAlpha: 1,
+    delay: 2.5,
+    duration: 1.5,
+});
+
+gsap.to('.box1', {
+    ease: 'power4.inOut',
+    delay: 3,
+    duration: 1,
+    width: '0',
+})
+
+gsap.to('.box2', {
+    ease: 'power4.inOut',
+    delay: 3,
+    duration: 1,
+    width: '0',
+});
+
+gsap.to('.box1', {
+    ease: 'power4.inOut',
+    delay: 4,
+    duration: 1,
+    height: '0',
+})
+
+gsap.to('.box2', {
+    ease: 'power4.inOut',
+    delay: 4,
+    duration: 1,
+    height: '0',
+})
+
+gsap.to('.box1', {
+    ease: 'power4.inOut',
+    delay: 3.7,
+    duration: 1,
+    display: 'none',
+})
+
+gsap.to('.box2', {
+    ease: 'power4.inOut',
+    delay: 3.7,
+    duration: 1,
+    display: 'none',
+})
