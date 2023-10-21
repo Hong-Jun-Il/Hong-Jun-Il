112fa255b89f4d12323a727e67db15bf8e6fa72b
Junil Hong <grt1022@gamil.com>
2023-10-21 22:28:12 +0900

    website

--- website/gsap.js
+++ website/gsap.js
@@ -1,162 +1,190 @@
-    // intro gsap
+// intro gsap start
 
-    gsap.to('.vertical-line1', {
-        delay: 1, 
-        display: 'block',
+gsap.to('.vertical-line1', {
+    delay: 1,
+    display: 'block',
+});
+
+gsap.to('.vertical-line2', {
+    delay: 1,
+    display: 'block',
+});
+
+gsap.to('.horizontal-line1', {
+    delay: 2.25,
+    display: 'block',
+});
+
+gsap.to('.horizontal-line2', {
+    delay: 2.25,
+    display: 'block',
+});
+
+gsap.to('.vertical-line1', {
+    delay: .5,
+    ease: 'power4.inOut',
+    duration: 2,
+    height: '40vh',
+});
+
+gsap.to('.vertical-line2', {
+    delay: .5,
+    ease: 'power4.inOut',
+    duration: 2,
+    height: '40vh',
+});
+
+gsap.to('.horizontal-line1', {
+    delay: 2,
+    ease: 'power4.inOut',
+    duration: 2,
+    width: '45vw',
+});
+
+gsap.to('.horizontal-line2', {
+    delay: 2,
+    ease: 'power4.inOut',
+    duration: 2,
+    width: '45vw',
+});
+
+gsap.to('.block-top-text', {
+    delay: 3,
+    ease: 'power4.inOut',
+    duration: 1.5,
+    autoAlpha: 1,
+});
+
+gsap.to('.block-bottom-text', {
+    delay: 3.5,
+    ease: 'power4.inOut',
+    duration: 1.5,
+    autoAlpha: 1,
+});
+
+gsap.to('.center-line', {
+    delay: 4,
+    ease: 'power4.inOut',
+    duration: 2,
+    width: '40vw',
+});
+
+gsap.to('.center-line', {
+    delay: 4.2,
+    ease: 'power4.inOut',
+    duration: 2,
+    width: '100%',
+});
+
+gsap.to('.block-top', {
+    top: '-50%',
+    ease: 'power3.inOut',
+    delay: 7,
+    duration: 2,
+});
+
+gsap.to('.block-bottom', {
+    bottom: '-50%',
+    delay: 7,
+    ease: 'power3.inOut',
+    duration: 2,
+});
+
+gsap.to('.lines', {
+    delay: 7.2,
+    autoAlpha: 0,
+    ease: 'power3.inOut',
+    duration: 1,
+
+    onComplete: () => {
+        addBoxAnimation();
+        addTextAnimation();
+    }
+});
+
+function addBoxAnimation() {
+    const revealSpan = document.querySelectorAll('#section1 .section-content .text-wrapper .h1-wrapper span');
+    let delay = 0.5;
+
+    revealSpan.forEach((span) => {
+        span.style.animation = 'text_reveal_box 1s ease forwards';
+        span.style.animationDelay = `${delay}s`;
+        delay += 1;
     });
+}
 
-    gsap.to('.vertical-line2', {
-        delay: 1, 
-        display: 'block',
-    });
+function addTextAnimation() {
+    const revealText = document.querySelectorAll('#section1 .section-content .text-wrapper .h1-wrapper h1');
+    let delay = 1;
 
-    gsap.to('.horizontal-line1', {
-        delay: 2.25,
-        display: 'block',
-    });
-
-    gsap.to('.horizontal-line2', {
-        delay: 2.25,
-        display: 'block',
-    });
-
-    gsap.to('.vertical-line1', {
-        delay: .5,
-        ease: 'power4.inOut',
-        duration: 2,
-        height: '40vh',
-    });
-
-    gsap.to('.vertical-line2', {
-        delay: .5,
-        ease: 'power4.inOut',
-        duration: 2,
-        height: '40vh',
-    });
-
-    gsap.to('.horizontal-line1', {
-        delay: 2,
-        ease: 'power4.inOut',
-        duration: 2,
-        width: '45vw',
-    });
-
-    gsap.to('.horizontal-line2', {
-        delay: 2,
-        ease: 'power4.inOut',
-        duration: 2,
-        width: '45vw',
-    });
-
-    gsap.to('.block-top-text', {
-        delay: 3, 
-        ease: 'power4.inOut',
-        duration: 1.5,
-        autoAlpha: 1,
-    });
-
-    gsap.to('.block-bottom-text', {
-        delay: 3.5, 
-        ease: 'power4.inOut',
-        duration: 1.5,
-        autoAlpha: 1,
-    });
-
-    gsap.to('.center-line', {
-        delay: 4, 
-        ease: 'power4.inOut',
-        duration: 2,
-        width: '40vw',
-    });
-
-    gsap.to('.center-line', {
-        delay: 4.2, 
-        ease: 'power4.inOut',
-        duration: 2,
-        width: '100%',
-    });
-
-    gsap.to('.block-top', {
-        top: '-50%',
-        ease: 'power3.inOut',
-        delay: 7,
-        duration: 2,
-    });
-
-    gsap.to('.block-bottom', {
-        bottom: '-50%',
-        delay: 7,
-        ease: 'power3.inOut',
-        duration: 2,
-    });
-
-    gsap.to('.lines', {
-        delay: 7.2,
-        autoAlpha: 0,
-        ease: 'power3.inOut',
-        duration: 1,
-    });
-
-    // section1 gsap
-
-    gsap.to('.border-right', {
-        ease: 'power4.inOut',
-        height: '100vh', 
-        duration: 1.5,
-        delay: 7.8,
-    });
-
-    gsap.to('.border-bottom', {
-        ease: 'power4.inOut',
-        width: '100vw', 
-        duration: 1.5,
-        delay: 7.8,
-    });
-
-    gsap.fromTo('.text1', {
-        autoAlpha: 0,
-        x: -50,
-    },{
-        autoAlpha: 1,
-        x: 0,
-        ease: 'power1.inOut',
-        duration: .8,
-        delay: 8,
-    });
-
-    gsap.fromTo('.text2', {
-        autoAlpha: 0,
-        x: -50,
-    },{
-        autoAlpha: 1,
-        x: 0,
-        ease: 'power1.inOut',
-        duration: .8,
-        delay: 8.8,
-    });
-
-    gsap.fromTo('.text3', {
-        autoAlpha: 0,
-        x: -50,
-    },{
-        autoAlpha: 1,
-        x: 0,
-        ease: 'power1.inOut',
-        duration: .8,
-        delay: 9.6,
+    revealText.forEach((h1) => {
+        h1.style.animation = 'text_reveal 1s ease forwards';
+        h1.style.animationDelay = `${delay}s`;
+        delay += 1;
     })
 
-    gsap.fromTo('.nav-list li', {
-        y: -40,
-        autoAlpha: 0,
-    },{
-        y: 0,
-        autoAlpha: 1,
-        duration: .8,
+}
+
+// intro gsap end
+
+// section1 gsap start
+
+// section1 gsap end
+
+// section2 gsap
+
+function aboutSecAni() {
+    gsap.from('.letter', {
+        duration: .4,
+        opacity: 0,
+        stagger: .1,
         ease: 'power1.inOut',
-        delay: (index)=>{
-            return (index * .1)+10;
+        scrollTrigger: {
+            trigger: '.letter',
+            start: 'top 95%',
         }
     })
 
-    
+    gsap.to('.line', {
+        duration: 1,
+        opacity: 1,
+        width: '82%',
+        delay: .3,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            trigger: '.letter',
+            start: 'top 75%',
+        }
+    })
+
+    gsap.from('#section2 .text-wrapper', {
+        duration: 1,
+        opacity: 0,
+        delay: .15,
+        y: 50,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            trigger: '#section2 .text-wrapper',
+            start: 'top 80%',
+        }
+    })
+}
+
+function projectSecAni(){
+    gsap.from('.project-item', {
+        duration: 3,
+        delay: -.5,
+        opacity: 0,
+        y: 70,
+        stagger: .15,
+        ease: 'elastic.inOut',
+        scrollTrigger: {
+            trigger: '#section3 .projects',
+            start: 'top 90%',
+
+        }
+    })
+}
+
+aboutSecAni();
+projectSecAni();
--- website/website.js
+++ website/website.js
@@ -1,44 +1,141 @@
-const sec1 = document.querySelector('.section1');
+// const sec1 = document.querySelector('.section1');
 
-sec1.style.cursor = 'none';
+// sec1.style.cursor = 'none';
 
-const cursor = document.createElement('div');
-cursor.classList.add('cursor');
-sec1.appendChild(cursor);
+// const cursor = document.createElement('div');
+// cursor.classList.add('cursor');
+// sec1.appendChild(cursor);
 
-const follow = document.createElement('div');
-follow.classList.add('follow');
-sec1.appendChild(follow);
+// const follow = document.createElement('div');
+// follow.classList.add('follow');
+// sec1.appendChild(follow);
 
-function move(obj, event){
-    obj.style = '';
-    obj.style.top = `${event.clientY}px`;
-    obj.style.left = `${event.clientX}px`;
+// function move(obj, event){
+//     obj.style = '';
+//     obj.style.top = `${event.clientY}px`;
+//     obj.style.left = `${event.clientX}px`;
+// }
+
+// if(cursor){
+//     sec1.addEventListener('mousemove', (event) =>{
+//         const e = event;
+//         const t = event.target;
+//         const f = follow;
+//         const c = cursor;
+
+//         if(t.tagName === 'A'){
+//             c.style.backgroundColor = "transparent";
+
+//             const rect = t.getBoundingClientRect();
+//             f.style.top = rect.top + 'px';
+//             f.style.left = rect.left + 'px';
+//             f.style.width = rect.width + 'px';
+//             f.style.height = rect.height + 'px';
+
+//             f.classList.add('on-focus');
+//         }
+//         else{
+//             move(f, e);
+//             move(c, e);
+
+//             f.classList.remove('on-focus');
+//         }
+//     })
+// }
+
+function jumpingAnimation() {
+    const jumps = document.querySelectorAll('.jumping-letters');
+
+    jumps.forEach((jump) => {
+        jump.addEventListener('mouseover', () => {
+            jump.classList.add('jump');
+        });
+    });
+
+    jumps.forEach((jump) => {
+        jump.addEventListener('animationend', () => {
+            jump.classList.remove('jump');
+        });
+    });
 }
 
-if(cursor){
-    sec1.addEventListener('mousemove', (event) =>{
-        const e = event;
-        const t = event.target;
-        const f = follow;
-        const c = cursor;
+function cursorShowImg(){
+    const sec3 = document.querySelector('#section3');
+    const c = document.querySelector('.project-cursor');
+    const projectItem = document.querySelectorAll('.project-item');
+    const div = document.querySelectorAll('.project-cursor > div');
+    const img = document.querySelectorAll('.bottom img')
 
-        if(t.tagName === 'A'){
-            c.style.backgroundColor = "transparent";
-        
-            const rect = t.getBoundingClientRect();
-            f.style.top = rect.top + 'px';
-            f.style.left = rect.left + 'px';
-            f.style.width = rect.width + 'px';
-            f.style.height = rect.height + 'px';
-        
-            f.classList.add('on-focus');
-        }
-        else{
-            move(f, e);
-            move(c, e);
+    sec3.addEventListener('mousemove', (e) => {
+        let x = e.clientX;
+        let y = e.clientY;
 
-            f.classList.remove('on-focus');
-        }
-    })
+        c.style.transform = `translate(${x}px, ${y}px)`;
+
+        projectItem.forEach((e) => {
+            e.addEventListener('mouseenter', (e) => {
+                if (e.target === projectItem[0]) {
+                    img.forEach((e) => {
+                        e.style.animation = 'an 5s linear infinite';
+                        e.style.animationDelay = e.dataset.delay;
+                    });
+                    div[0].classList.add('visible');
+                }
+                else if (e.target === projectItem[1]) {
+                    img.forEach((e) => {
+                        e.style.animation = 'an 5s linear infinite';
+                        e.style.animationDelay = e.dataset.delay;
+                    });
+                    div[1].classList.add('visible');
+                }
+                else if (e.target === projectItem[2]) {
+                    img.forEach((e) => {
+                        e.style.animation = 'an 5s linear infinite';
+                        e.style.animationDelay = e.dataset.delay;
+                    });
+                    div[2].classList.add('visible');
+                }
+                else if (e.target === projectItem[3]) {
+                    img.forEach((e) => {
+                        e.style.animation = 'an 5s linear infinite';
+                        e.style.animationDelay = e.dataset.delay;
+                    });
+                    div[3].classList.add('visible');
+                }
+                else if (e.target === projectItem[4]) {
+                    img.forEach((e) => {
+                        e.style.animation = 'an 5s linear infinite';
+                        e.style.animationDelay = e.dataset.delay;
+                    });
+                    div[4].classList.add('visible');
+                }
+                else if (e.target === projectItem[5]) {
+                    img.forEach((e) => {
+                        e.style.animation = 'an 5s linear infinite';
+                        e.style.animationDelay = e.dataset.delay;
+                    });
+                    div[5].classList.add('visible');
+                }
+                else if (e.target === projectItem[6]) {
+                    img.forEach((e) => {
+                        e.style.animation = 'an 5s linear infinite';
+                        e.style.animationDelay = e.dataset.delay;
+                    });
+                    div[6].classList.add('visible');
+                }
+            })
+
+            e.addEventListener('mouseleave', (e) => {
+                img.forEach((e) => {
+                    e.style.animation = '';
+                })
+                div.forEach((e) => {
+                    e.classList.remove('visible');
+                })
+            })
+        })
+    });
 }
+
+jumpingAnimation();
+// cursorShowImg();
