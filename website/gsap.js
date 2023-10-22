8a7b1d924fe7d4ab527aeee40be10cf4ab6547b6
Junil Hong <grt1022@gamil.com>
2023-10-22 23:34:35 +0900

    website

--- website/gsap.js
+++ website/gsap.js
@@ -1,106 +1,110 @@
-// intro gsap start
+// intro gsap
 
-gsap.to('.vertical-line1', {
-    delay: 1,
-    display: 'block',
-});
+function introAnimation() {
+    gsap.to('.vertical-line1', {
+        delay: 1,
+        display: 'block',
+    });
 
-gsap.to('.vertical-line2', {
-    delay: 1,
-    display: 'block',
-});
+    gsap.to('.vertical-line2', {
+        delay: 1,
+        display: 'block',
+    });
 
-gsap.to('.horizontal-line1', {
-    delay: 2.25,
-    display: 'block',
-});
+    gsap.to('.horizontal-line1', {
+        delay: 2.25,
+        display: 'block',
+    });
 
-gsap.to('.horizontal-line2', {
-    delay: 2.25,
-    display: 'block',
-});
+    gsap.to('.horizontal-line2', {
+        delay: 2.25,
+        display: 'block',
+    });
 
-gsap.to('.vertical-line1', {
-    delay: .5,
-    ease: 'power4.inOut',
-    duration: 2,
-    height: '40vh',
-});
+    gsap.to('.vertical-line1', {
+        delay: .5,
+        ease: 'power4.inOut',
+        duration: 2,
+        height: '40vh',
+    });
 
-gsap.to('.vertical-line2', {
-    delay: .5,
-    ease: 'power4.inOut',
-    duration: 2,
-    height: '40vh',
-});
+    gsap.to('.vertical-line2', {
+        delay: .5,
+        ease: 'power4.inOut',
+        duration: 2,
+        height: '40vh',
+    });
 
-gsap.to('.horizontal-line1', {
-    delay: 2,
-    ease: 'power4.inOut',
-    duration: 2,
-    width: '45vw',
-});
+    gsap.to('.horizontal-line1', {
+        delay: 2,
+        ease: 'power4.inOut',
+        duration: 2,
+        width: '45vw',
+    });
 
-gsap.to('.horizontal-line2', {
-    delay: 2,
-    ease: 'power4.inOut',
-    duration: 2,
-    width: '45vw',
-});
+    gsap.to('.horizontal-line2', {
+        delay: 2,
+        ease: 'power4.inOut',
+        duration: 2,
+        width: '45vw',
+    });
 
-gsap.to('.block-top-text', {
-    delay: 3,
-    ease: 'power4.inOut',
-    duration: 1.5,
-    autoAlpha: 1,
-});
+    gsap.to('.block-top-text', {
+        delay: 3,
+        ease: 'power4.inOut',
+        duration: 1.5,
+        autoAlpha: 1,
+    });
 
-gsap.to('.block-bottom-text', {
-    delay: 3.5,
-    ease: 'power4.inOut',
-    duration: 1.5,
-    autoAlpha: 1,
-});
+    gsap.to('.block-bottom-text', {
+        delay: 3.5,
+        ease: 'power4.inOut',
+        duration: 1.5,
+        autoAlpha: 1,
+    });
 
-gsap.to('.center-line', {
-    delay: 4,
-    ease: 'power4.inOut',
-    duration: 2,
-    width: '40vw',
-});
+    gsap.to('.center-line', {
+        delay: 4,
+        ease: 'power4.inOut',
+        duration: 2,
+        width: '40vw',
+    });
 
-gsap.to('.center-line', {
-    delay: 4.2,
-    ease: 'power4.inOut',
-    duration: 2,
-    width: '100%',
-});
+    gsap.to('.center-line', {
+        delay: 4.2,
+        ease: 'power4.inOut',
+        duration: 2,
+        width: '100%',
+    });
 
-gsap.to('.block-top', {
-    top: '-50%',
-    ease: 'power3.inOut',
-    delay: 7,
-    duration: 2,
-});
+    gsap.to('.block-top', {
+        top: '-50%',
+        ease: 'power3.inOut',
+        delay: 7,
+        duration: 2,
+    });
 
-gsap.to('.block-bottom', {
-    bottom: '-50%',
-    delay: 7,
-    ease: 'power3.inOut',
-    duration: 2,
-});
+    gsap.to('.block-bottom', {
+        bottom: '-50%',
+        delay: 7,
+        ease: 'power3.inOut',
+        duration: 2,
+    });
 
-gsap.to('.lines', {
-    delay: 7.2,
-    autoAlpha: 0,
-    ease: 'power3.inOut',
-    duration: 1,
+    gsap.to('.lines', {
+        delay: 7.2,
+        autoAlpha: 0,
+        ease: 'power3.inOut',
+        duration: 1,
 
-    onComplete: () => {
-        addBoxAnimation();
-        addTextAnimation();
-    }
-});
+        onComplete: () => {
+            addBoxAnimation();
+            addTextAnimation();
+        }
+    });
+}
+
+// section1 gsap
 
 function addBoxAnimation() {
     const revealSpan = document.querySelectorAll('#section1 .section-content .text-wrapper .h1-wrapper span');
@@ -125,34 +129,28 @@ function addTextAnimation() {
 
 }
 
-// intro gsap end
-
-// section1 gsap start
-
-// section1 gsap end
-
 // section2 gsap
 
 function aboutSecAni() {
-    gsap.from('.letter', {
+    gsap.from('#section2 .letter', {
         duration: .4,
         opacity: 0,
         stagger: .1,
         ease: 'power1.inOut',
         scrollTrigger: {
-            trigger: '.letter',
+            trigger: '#section2 .letter',
             start: 'top 95%',
         }
     })
 
-    gsap.to('.line', {
+    gsap.to('#section2 .line', {
         duration: 1,
         opacity: 1,
         width: '82%',
         delay: .3,
         ease: 'power1.inOut',
         scrollTrigger: {
-            trigger: '.letter',
+            trigger: '#section2 .letter',
             start: 'top 75%',
         }
     })
@@ -170,21 +168,45 @@ function aboutSecAni() {
     })
 }
 
-function projectSecAni(){
-    gsap.from('.project-item', {
-        duration: 3,
-        delay: -.5,
-        opacity: 0,
-        y: 70,
-        stagger: .15,
-        ease: 'elastic.inOut',
-        scrollTrigger: {
-            trigger: '#section3 .projects',
-            start: 'top 90%',
+// section3 gsap 
 
+function projectSecAni() {
+    gsap.from('#section3 .letter', {
+        duration: .4,
+        opacity: 0,
+        stagger: .1,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            trigger: '#section3 .letter',
+            start: 'top 95%',
         }
     })
+
+    gsap.to('#section3 .line', {
+        duration: 1,
+        opacity: 1,
+        width: '82%',
+        delay: .3,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            trigger: '#section3 .letter',
+            start: 'top 75%',
+        }
+    })
+    document.querySelectorAll('.project-item').forEach(item => {
+        gsap.from(item, {
+            opacity: 0,
+            duration: .8,
+            scale: 0,
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: item,
+                start: 'top 90%',
+            }
+        })
+    });
 }
 
-aboutSecAni();
-projectSecAni();
+// introAnimation();
+// aboutSecAni();
+// projectSecAni();
--- website/website.js
+++ website/website.js
@@ -1,47 +1,9 @@
-// const sec1 = document.querySelector('.section1');
-
-// sec1.style.cursor = 'none';
-
-// const cursor = document.createElement('div');
-// cursor.classList.add('cursor');
-// sec1.appendChild(cursor);
-
-// const follow = document.createElement('div');
-// follow.classList.add('follow');
-// sec1.appendChild(follow);
-
-// function move(obj, event){
-//     obj.style = '';
-//     obj.style.top = `${event.clientY}px`;
-//     obj.style.left = `${event.clientX}px`;
-// }
-
-// if(cursor){
-//     sec1.addEventListener('mousemove', (event) =>{
-//         const e = event;
-//         const t = event.target;
-//         const f = follow;
-//         const c = cursor;
-
-//         if(t.tagName === 'A'){
-//             c.style.backgroundColor = "transparent";
-
-//             const rect = t.getBoundingClientRect();
-//             f.style.top = rect.top + 'px';
-//             f.style.left = rect.left + 'px';
-//             f.style.width = rect.width + 'px';
-//             f.style.height = rect.height + 'px';
-
-//             f.classList.add('on-focus');
-//         }
-//         else{
-//             move(f, e);
-//             move(c, e);
-
-//             f.classList.remove('on-focus');
-//         }
-//     })
-// }
+function locoInitailze(){
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('.section'),
+        smooth: true
+    });
+}
 
 function jumpingAnimation() {
     const jumps = document.querySelectorAll('.jumping-letters');
@@ -62,7 +24,7 @@ function jumpingAnimation() {
 function cursorShowImg(){
     const sec3 = document.querySelector('#section3');
     const c = document.querySelector('.project-cursor');
-    const projectItem = document.querySelectorAll('.project-item');
+    const projectItem = document.querySelectorAll('#section3 .col .project-item');
     const div = document.querySelectorAll('.project-cursor > div');
     const img = document.querySelectorAll('.bottom img')
 
@@ -102,27 +64,6 @@ function cursorShowImg(){
                     });
                     div[3].classList.add('visible');
                 }
-                else if (e.target === projectItem[4]) {
-                    img.forEach((e) => {
-                        e.style.animation = 'an 5s linear infinite';
-                        e.style.animationDelay = e.dataset.delay;
-                    });
-                    div[4].classList.add('visible');
-                }
-                else if (e.target === projectItem[5]) {
-                    img.forEach((e) => {
-                        e.style.animation = 'an 5s linear infinite';
-                        e.style.animationDelay = e.dataset.delay;
-                    });
-                    div[5].classList.add('visible');
-                }
-                else if (e.target === projectItem[6]) {
-                    img.forEach((e) => {
-                        e.style.animation = 'an 5s linear infinite';
-                        e.style.animationDelay = e.dataset.delay;
-                    });
-                    div[6].classList.add('visible');
-                }
             })
 
             e.addEventListener('mouseleave', (e) => {
@@ -137,5 +78,6 @@ function cursorShowImg(){
     });
 }
 
-jumpingAnimation();
+// locoInitailze();
+// jumpingAnimation();
 // cursorShowImg();
